3ebd20b749fd515e9a76003eb6a56b07262b037f
Victor Hugo <victorhugofraga574@gmail.com>
2024-09-29 13:42:34 -0300

    tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+     {
+        //tipos primitivos
+        int salario = 2500;
+        double salario2 = 2500.00;
+
+        //tipo de texto
+        String nome ="Pablo";
+
+        
     }
 }
