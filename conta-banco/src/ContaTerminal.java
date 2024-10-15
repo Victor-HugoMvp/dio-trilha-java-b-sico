737ed383c1ec47b9a9918b8f0f738118b7f3a49a
Victor Hugo <victorhugofraga574@gmail.com>
2024-10-15 20:17:17 -0300

    Terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int numero;
+        String texto;
+        String NomeCliente;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da Agência!");
+        
     }
 }
